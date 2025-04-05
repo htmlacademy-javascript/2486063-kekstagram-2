a2311e7a3f0e6a04c97e0815969fd2b4684a05b2
Keks <keks@htmlacademy.ru>
2025-04-05 17:20:18 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/thumbnail.js
@@ -0,0 +1,32 @@
+import { openBigPicture } from './big-picture.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const createThumbnail = (pictureData) => {
+  const { url, description, likes, comments } = pictureData;
+  const thumbnail = pictureTemplate.cloneNode(true);
+  const image = thumbnail.querySelector('.picture__img');
+  image.src = url;
+  image.alt = description;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openBigPicture(pictureData);
+  });
+
+  return thumbnail;
+};
+
+const renderThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+  picturesContainer.append(fragment);
+};
+
+export { renderThumbnails };
